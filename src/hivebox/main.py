952982ae67a81efa3cb5e9ead12440f3523c1c82
Mateus <mhenrique4060@gmail.com>
2026-09-07 17:40:42 -0300

    feat: implement temperature endpoint

--- src/hivebox/main.py
+++ src/hivebox/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
-from version import main
+from .version import main
+import datetime
 
 app = FastAPI()
 
 @app.get("/version")
-def return_version():
+async def return_version():
     result = main()
     return result
+
+
+@app.get("/temperature")
+async def return_temp():
+    now = datetime.datetime.now()
+    opensense = f"https://api.opensensemap.org/boxes?date={now}&phenomenon=temperature&format=:json"
+    response = await client.get(opensense)
+    return response
